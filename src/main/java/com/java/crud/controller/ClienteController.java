2973698b7dfec812cd20201b0ae67dff19a0c835
Marcelo Melo <marceloagmelo@gmail.com>
2025-04-28 09:28:57 -0300

    Ajustes

--- src/main/java/com/java/crud/controller/ClienteController.java
+++ src/main/java/com/java/crud/controller/ClienteController.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +32,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping("/clientes")
 @Tag(name = "open-api")
-@CrossOrigin
+
 public class ClienteController {
 
     @Autowired
